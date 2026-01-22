7a98c949b1f21b9427f944077912c039bf1fce99
Luis Fortuna <javier-uribe@hotmail.com>
2026-01-22 19:44:59 -0400

    disenando mi formulario

--- EjercicioPractico1/EjercicioPractico1/Form1.Designer.cs
+++ EjercicioPractico1/EjercicioPractico1/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(55, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(55, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(433, 47);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(500, 57);
+            button1.Name = "button1";
+            button1.Size = new Size(376, 170);
+            button1.TabIndex = 2;
+            button1.Text = "Enviar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(913, 288);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Primer Ejercicio";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
